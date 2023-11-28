785733f36ba39dfcf1012ead6ebaff71eca3ab19
ParthAgarwal09 <deepg.ag.77@gmail.com>
2023-11-28 11:27:42 +0530

    new1

--- async-programming/promise/index.js
+++ async-programming/promise/index.js
@@ -2,8 +2,8 @@ let pr = new Promise((resolve,reject)=>{
     setTimeout(()=>{
         let data = "promise data resolve"
         resolve(data);
-        // let err = "new error"
-        // reject(err);
+        let err = "new error"
+        reject(err);
     },5000)
 })
 
@@ -11,5 +11,10 @@ let pr = new Promise((resolve,reject)=>{
 
 pr
     .then((data)=>{
-        
+        console.log(data);
+
+    })
+    .catch((y)=>{
+        console.log(y);
+
     })
