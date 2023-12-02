a8a70170cb5bfab7edcd3472754d9202f7fa5912
ParthAgarwal09 <deepg.ag.77@gmail.com>
2023-12-02 09:21:12 +0530

    new3

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+const head = document.getElementsByTagName('h1')[0];
+console.log(head);
+
+head.style.color = 'red';
+head.style.backgroundColor='blue';
+
+const para = document.getElementsById('para');
